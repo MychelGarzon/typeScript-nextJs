c2aed43f63d613f8109d5482d8ec4ca256db61e9
mtjomy17@gmail.com <mtjomy17@gmail.com>
2024-03-08 14:57:03 +0200

    Fix card styling and display country official and native names

--- src/app/countries/CountriesAPI.tsx
+++ src/app/countries/CountriesAPI.tsx
@@ -42,14 +42,22 @@ const CountriesAPI = () => {
     return (
         <div>
            {countries.map((country, index) => (
-            <Card key={`${country.capital}_${index}`}>
+            <Card style={{ width: '18rem' }}key={`${country.capital}_${index}`}>
                 <div key={`${country.capital[0]}${index}`}>
             <h2>{country.capital[0]}</h2>
-      <p> Country name: {country.name.common}
-      </p>
+      <p> Country Official name: {country.name.official} </p>
+     
+      {Object.entries(country.name.nativeName).map(([key, value]) => (
+    <div key={key}>
+            {key}: {value.common}
+        <p>{value.common}</p>
+        <p>{value.official}</p>
+    </div>
+))}
     </div>
   </Card>
-))}
+))} 
+    
         </div>
     );
 }
